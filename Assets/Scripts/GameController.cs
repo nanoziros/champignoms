8623072903392d6e286f058c86ad7254d0e9377c
Juan Diaz Gutierrez <juan.rod.diaz@gmail.com>
2023-02-03 23:27:01 +0100

    Adding init to gameplay, more logic.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelData
+{
+    public string Name;
+    public GameObject GamePrefab;
+    public Sprite MenuImage;
+    public Vector2 TimeBonusCellPosition;
+    public AudioClip Music;
+}
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField]
+    private SceneLoader _sceneLoader;
+    [SerializeField]
+    private SoundController _soundController;
+    [Header("Audio")]
+    [SerializeField]
+    private AudioListener _audioListener;
+    [SerializeField]
+    private AudioClip _menuMusic;
+
+    [SerializeField]
+    private List<LevelData> _levelData;
+
+    public static GameController Instance { get; private set; }
+    public SceneLoader SceneLoader { get { return _sceneLoader; } }
+
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("GameController");
+        if (objs.Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+
+        _sceneLoader.Initialize();
+    }
+
+    private List<string> GetAllLevelNames()
+    {
+        var list = new List<string>();
+        foreach (var entry in _levelData)
+        {
+            list.Add(entry.Name);
+        }
+        return list;
+    }
+
+    public List<Sprite> GetMenuLevelSprites()
+    {
+        var list = new List<Sprite>();
+        foreach (var entry in _levelData)
+        {
+            list.Add(entry.MenuImage);
+        }
+        return list;
+    }
+
+    public string GetLevelNameByIdx(int idx)
+    {
+        if (idx >= 0 && idx < _levelData.Count)
+        {
+            return _levelData[idx].Name;
+        }
+        Debug.LogError("Wrong index for level name!");
+        return "???";
+    }
+
+    public void LoadGameplayScenario()
+    {
+        _soundController.Stop();
+        _sceneLoader.LoadGameplayScene();
+    }
+
+    public void LoadMainMenu()
+    {
+        _soundController.Play(_menuMusic);
+        _sceneLoader.LoadMainMenu();
+    }
+
+    public void DeleteData()
+    {
+        _sceneLoader.ReloadScene();
+    }
+
+    public void ToggleMusicEnabled()
+    {
+        _soundController.IsEnabled = !_soundController.IsEnabled;
+    }
+}
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField]
+    private float _fadeOutTime = 1.5f;
+
+    private AudioSource _audioMusicSource;
+    private float _defaultVolume = 0.0f;
+
+    public bool IsEnabled { get => _audioMusicSource.enabled; set => _audioMusicSource.enabled = value; }
+
+    void Start()
+    {
+        _audioMusicSource = GetComponent<AudioSource>();
+        _defaultVolume = _audioMusicSource.volume;
+    }    
+
+    public void Play(AudioClip clip)
+    {
+        StopCoroutine(FadeOut());
+        _audioMusicSource.volume = _defaultVolume;
+        _audioMusicSource.clip = clip;
+        _audioMusicSource.Play();
+    }
+
+    public void Stop(bool fadeOut = true)
+    {
+        if (fadeOut)
+        {
+            StartCoroutine(FadeOut());
+            return;
+        }
+        _audioMusicSource.Stop();
+    }
+
+    public IEnumerator FadeOut()
+    {
+        while (_audioMusicSource.volume > 0)
+        {
+            _audioMusicSource.volume -= _defaultVolume * Time.unscaledDeltaTime / _fadeOutTime;
+
+            yield return null;
+        }
+
+        _audioMusicSource.Stop();
+        _audioMusicSource.volume = _defaultVolume;
+    }
+}
