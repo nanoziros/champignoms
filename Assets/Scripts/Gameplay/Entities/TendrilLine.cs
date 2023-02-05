558307e655bea615d15ee26e79588ca41d6a673e
nanoziros <nero.lucho92@gmail.com>
2023-02-05 14:52:00 +0100

    Tendril logic variation

--- Assets/Scripts/Gameplay/Entities/TendrilLine.cs
+++ Assets/Scripts/Gameplay/Entities/TendrilLine.cs
@@ -5,23 +5,68 @@ namespace Gameplay.Entities
 {
     public class TendrilLine : MonoBehaviour
     {
-        [SerializeField] int subdivisions = 2;
+        [SerializeField] int maxSubdivisions = 20;
+        [SerializeField] float maxDistanceSubdivisions = 40;
+        [SerializeField] float variancePosition = 10;
         [SerializeField] LineRenderer lineRenderer;
-        Tween _growthAnimation;
         
-        public Tween GrowNode(TendrilNode originNode, TendrilNode targetNode, float growthSpeed)
+        Sequence _growthSequence;
+        
+        public Sequence GrowNode(TendrilNode originNode, TendrilNode targetNode, float growthSpeed)
         {
             var originPosition = originNode.transform.position;
-            var targetPosition = targetNode.transform.position;
-            
-            lineRenderer.SetPosition(0, originPosition);
-            lineRenderer.SetPosition(1, originPosition);
+            var finalPosition = targetNode.transform.position;
 
-            _growthAnimation = DOTween
-                .To(() => lineRenderer.GetPosition(1), x => lineRenderer.SetPosition(1, x), targetPosition, growthSpeed)
-                .SetEase(Ease.Linear).SetSpeedBased().Play();
+            var normalizedDistance = Mathf.Clamp01(Vector3.Distance(originPosition, finalPosition) / maxDistanceSubdivisions);
+            var subDivisions = Mathf.CeilToInt(Mathf.Lerp(2, maxSubdivisions, normalizedDistance));
+
+            var targetPositions = new Vector3[subDivisions];
+            for (var i = 0; i < subDivisions; i++)
+            {
+                var normalizedIndex = i / (float) subDivisions;
+                var interpolatedPosition = Vector3.Lerp(originPosition, finalPosition, normalizedIndex);
+
+                if (i != subDivisions - 1)
+                {
+                    var variance = new Vector3(Random.Range(-variancePosition, variancePosition),
+                        Random.Range(-variancePosition, variancePosition), 0);
+                    targetPositions[i] = interpolatedPosition + variance;
+                }
+                else
+                {
+                    targetPositions[i] = finalPosition;
+                }
+            }
             
-            return _growthAnimation;
+            var targetSpeeds = new float[subDivisions];
+            for (var i = 1; i < subDivisions; i++)
+            {
+                var distance = Vector3.Distance(targetPositions[i], targetPositions[i - 1]);
+                targetSpeeds[i] = distance / growthSpeed;
+            }
+            
+            _growthSequence = DOTween.Sequence();
+            for (var i = 1; i < subDivisions; i++)
+            {
+                var currentMaxIndex = i;
+                _growthSequence.AppendCallback(() =>
+                {
+                    lineRenderer.positionCount = currentMaxIndex + 1;
+                    for (var j = 0; j < currentMaxIndex; j++)
+                    {
+                        lineRenderer.SetPosition(j, targetPositions[j]);
+                    }
+                });
+                _growthSequence.AppendCallback(() =>
+                {
+                    lineRenderer.SetPosition(currentMaxIndex, targetPositions[currentMaxIndex-1]);
+                });
+                _growthSequence.Append(DOTween
+                    .To(() => lineRenderer.GetPosition(currentMaxIndex), x => lineRenderer.SetPosition(currentMaxIndex, x), targetPositions[currentMaxIndex],
+                        targetSpeeds[currentMaxIndex]).SetEase(Ease.Linear));
+            }
+            
+            return _growthSequence;
         }
     }
 }
